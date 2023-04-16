9f07912af1c4e71713fc13d4d858d0a9e9ef12bf
Brandon <brandon.cheung.dev@gmail.com>
2023-04-16 19:18:58 -0400

    adds sorting history by most recent first

--- api/middleware/calculateAndUpdateAllFields.ts
+++ api/middleware/calculateAndUpdateAllFields.ts
@@ -12,26 +12,38 @@ const recalculateTotal = async (
       firebaseUserUid: req.firebaseUid,
     });
     if (currDocument) {
+      // recalculate remainder
       currDocument.current.budget.remaining = currDocument.current.budget.total;
       currDocument.current.needs.remaining = currDocument.current.needs.total;
       currDocument.current.wants.remaining = currDocument.current.wants.total;
       currDocument.current.savings.remaining =
         currDocument.current.savings.total;
 
-      currDocument?.categories[BudgetCategory.Needs].forEach((category) => {
+      currDocument.categories[BudgetCategory.Needs].forEach((category) => {
         currDocument.current.budget.remaining -= category.amount;
         currDocument.current.needs.remaining -= category.amount;
       });
-      currDocument?.categories[BudgetCategory.Wants].forEach((category) => {
+      currDocument.categories[BudgetCategory.Wants].forEach((category) => {
         currDocument.current.budget.remaining -= category.amount;
         currDocument.current.wants.remaining -= category.amount;
       });
-      currDocument?.categories[BudgetCategory.Savings].forEach((category) => {
+      currDocument.categories[BudgetCategory.Savings].forEach((category) => {
         currDocument.current.budget.remaining -= category.amount;
         currDocument.current.savings.remaining -= category.amount;
       });
 
-      await currDocument?.save();
+      // sort arrays by date (most recent first)
+      currDocument.categories[BudgetCategory.Needs].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+      currDocument.categories[BudgetCategory.Wants].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+      currDocument.categories[BudgetCategory.Savings].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+
+      await currDocument.save();
 
       res.status(200).send("Ok");
     } else {
