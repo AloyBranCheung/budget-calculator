73bfb0d681094913910c8c20b7d1ff118ca37a5d
bcheung <brandon.cheung.dev@gmail.com>
2023-09-08 08:05:51 -0400

    updates test app script

--- test_app.sh
+++ test_app.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
-echo("Changing working directory to api/")
+function console_message () {
+    local PURPLE='\x1B[0;35m'
+    local NC='\x1B[0m'
+    local upper_string=$(echo "\ *****$1*****" | tr '[:lower:]' '[:upper:]')
+    echo -e "\e[1m${PURPLE}$upper_string${NC}\e[0m"
+}
+
+# api
+console_message "Changing working directory to api"
 cd api
-echo("Linting api")
+# eslint
+console_message "Linting api"
 yarn lint
-echo("Testing api")
-echo("Changing working directory to client/")
+# test
+console_message "Testing api"
+
+# client
+console_message "Changing working directory to client"
 cd ../client
-echo("Linting client")
+
+#eslint
+console_message "Linting client"
 yarn lint 
-echo("Testing client")
+
+console_message "Testing client"
+# jest tests
+console_message "Running jest tests"
+yarn jest ./__tests__
+# playwright tests
+console_message "Running playwright e2e tests"
+yarn test-e2e
