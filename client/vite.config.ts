f6c419cdd7f025d37fbf5468beaa080f81eee7cc
bcheung <brandon.cheung.dev@gmail.com>
2023-09-07 21:06:40 -0400

    adds hot reload

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    // open: true,
+    hmr: {
+      clientPort: 3000,
+    },
+    watch: {
+      usePolling: true,
+    },
   },
 });
