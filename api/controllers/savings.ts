48f126e8ac168c3119dcc4c3d86b0f689ebdb57f
bcheung <brandon.cheung.dev@gmail.com>
2023-09-08 23:13:09 -0400

    adds create/update to savings goal

--- api/controllers/savings.ts
+++ api/controllers/savings.ts
@@ -1,14 +1,69 @@
-import { type NextFunction, type Request, type Response } from "express";
+import { type NextFunction, type Response } from "express";
+import { type RequestWithDecodedIdToken } from "../middleware/withAuth";
 // models
 import SavingsGoalModel from "../models/SavingsGoalModel";
 
 const addSavingsGoal = async (
-  req: Request,
+  req: RequestWithDecodedIdToken,
   res: Response,
   next: NextFunction
 ) => {
-  console.log("addSavingsGoal route hit");
-  res.send("Hello world");
+  const { firebaseUid } = req;
+  const {
+    currentAmountContributed,
+    descriptionOfGoal,
+    nameOfGoal,
+    targetAmount,
+  } = req.body;
+  if (!firebaseUid) {
+    next(new Error("No firebase uid"));
+  }
+  try {
+    await SavingsGoalModel.create({
+      firebaseUserUid: firebaseUid,
+      currentAmountContributed,
+      descriptionOfGoal,
+      nameOfGoal,
+      targetAmount,
+    });
+    res.status(200).send("OK");
+  } catch (error) {
+    console.log("Error creating savings goal.");
+    next(error);
+  }
 };
 
 export default addSavingsGoal;
+
+export const updateSavingsGoal = async (
+  req: RequestWithDecodedIdToken,
+  res: Response,
+  next: NextFunction
+) => {
+  const { firebaseUid } = req;
+  const {
+    currentAmountContributed,
+    descriptionOfGoal,
+    nameOfGoal,
+    targetAmount,
+  } = req.body;
+  if (!firebaseUid) {
+    next(new Error("No firebase uid"));
+  }
+  try {
+    await SavingsGoalModel.findOneAndUpdate(
+      {
+        firebaseUserUid: firebaseUid,
+      },
+      {
+        currentAmountContributed,
+        descriptionOfGoal,
+        nameOfGoal,
+        targetAmount,
+      }
+    );
+    res.status(200).send("OK");
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/authenticatedRoute.ts
+++ api/routes/authenticatedRoute.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import withAuth from "../middleware/withAuth";
 // controllers
-import addSavingsGoal from "../controllers/savings";
+import addSavingsGoal, { updateSavingsGoal } from "../controllers/savings";
 import {
   getBudgetData,
   updateBudgetData,
@@ -22,7 +22,8 @@ router.get("/data", getBudgetData);
 router.put("/data", updateBudgetData);
 router.delete("/data", resetBudgetData);
 
-router.post("/savings-goal", addSavingsGoal);
+router.post("/savings-goal", addSavingsGoal); // add
+router.put("/savings-goal/update", updateSavingsGoal); // update
 
 router.put("/data/categories", updateBudgetCategories);
 router.delete("/data/:category/:expenditureId", deleteExpenditure);
